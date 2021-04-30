ff568da7c7998bdcdc29fa000f7a0f7611d1b719
drva <elenacafnerna@gmail.com>
2021-04-30 14:44:13 -0400

    beginning notes

---
+++ ConvertEdXCoursePipeline.sh
@@ -0,0 +1,28 @@
+
+# ==CONTENT==
+
+# Run the main java converter
+
+# Convert LO's, if they exist
+
+# Move the LO files, if they exist, so they's in the same folder as the XSLT converter
+
+# Run the main xslt converter
+
+# (Optional) process errors from the above
+
+# Make the protooutline into an outline
+
+# Run inline activity java if needed (not needed for activities any more, still needed for submit and compare)
+
+# Do more stuff re submit and compare
+
+# Library content if it exists should be processed at some point?
+
+# Name changing if it's long enough (usually do after everything is valid when doing by hand, but)
+
+# ==PROBLEMS==
+
+# Run the multipart splitter
+
+# Run the xslt converter
