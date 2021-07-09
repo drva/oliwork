7b104b65876a3cbcc68cf29fc6e6f9a8218df7b1
drva <elenacafnerna@gmail.com>
2021-07-09 06:36:27 -0400

    also lists tags from the structure files

--- StanfordListTags.java
+++ StanfordListTags.java
@@ -19,9 +19,19 @@ public class StanfordListTags
 		HashSet<String> contentTags = new HashSet<String>();
 		HashSet<String> problemTags = new HashSet<String>();
 		
+		HashSet<String> courseTags = new HashSet<String>();
+		HashSet<String> chapterTags = new HashSet<String>();
+		HashSet<String> seqTags = new HashSet<String>();
+		HashSet<String> verticalTags = new HashSet<String>();
+		
 		//get the lists of files to process
 		File[] contentFiles = new File(args[0]+"/html").listFiles(); //https://stackoverflow.com/questions/4917326/how-to-iterate-over-the-files-of-a-certain-directory-in-java
 		File[] problemFiles = new File(args[0]+"/problem").listFiles();
+		
+		File[] courseFiles = new File(args[0]+"/course").listFiles();
+		File[] chapterFiles = new File(args[0]+"/chapter").listFiles();
+		File[] seqFiles = new File(args[0]+"/sequential").listFiles();
+		File[] verticalFiles = new File(args[0]+"/vertical").listFiles();
 		
 		//send them to process.
 		for(int i=0; i<contentFiles.length; i++)
@@ -33,6 +43,44 @@ public class StanfordListTags
 			processFile(problemFiles[i], problemTags); //by passing the hashset I can make one method and use it for both content and problems
 		}
 		
+		for(int i=0; i<courseFiles.length; i++)
+		{
+			processFile(courseFiles[i], courseTags); 
+		}
+		for(int i=0; i<chapterFiles.length; i++)
+		{
+			processFile(chapterFiles[i], chapterTags); 
+		}
+		for(int i=0; i<seqFiles.length; i++)
+		{
+			processFile(seqFiles[i], seqTags); 
+		}
+		for(int i=0; i<verticalFiles.length; i++)
+		{
+			processFile(verticalFiles[i], verticalTags); 
+		}
+		
+		
+		
+		System.out.println("## Higher structure tags:");
+		System.out.println("### -course-");
+		courseTags.forEach((String name) -> {
+            System.out.println(name);
+        });
+		System.out.println("### -chapter-");
+		chapterTags.forEach((String name) -> {
+            System.out.println(name);
+        });
+		System.out.println("### -sequential-");
+		seqTags.forEach((String name) -> {
+            System.out.println(name);
+        });
+		System.out.println("### -vertical-");
+		verticalTags.forEach((String name) -> {
+            System.out.println(name);
+        });
+		
+		System.out.println();
 		System.out.println("## Content tags:");
 		contentTags.forEach((String name) -> {
             System.out.println(name);
